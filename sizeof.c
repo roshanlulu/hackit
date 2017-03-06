aec5efd660a7a65154862ec8f16b840a14241ec0
Roshan Lulu <roshanlulu@gmail.com>
2017-03-06 15:35:52 +1100

    Size of

---
+++ sizeof.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+  int N = 10, apple;
+  int *q;
+  long long p;
+  p = N * sizeof * q;;
+  printf("%lld \n", p);
+  apple = sizeof (long long) * p;
+  printf("%d\n", apple);
+
+  return 0;
+}
